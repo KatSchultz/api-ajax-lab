a667f230389068737eebb654804c2771c2d02004
KatSchultz <ks.katieschultz@gmail.com>
2022-06-07 16:46:24 -0400

    displays first 10 posts, added stying

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 fetch("https://www.reddit.com/r/aww/.json")
   .then((res) => res.json())
   .then((jsonData) => {
-    console.log(jsonData.data.children);
     let posts = jsonData.data.children;
-    console.log(typeof posts);
-    console.log(posts[0]);
-    posts.forEach((post) => {
+    let first10 = posts.slice(0, 10);
+    console.log(first10);
+    first10.forEach((post) => {
+      //   console.log(post.data);
       const postDiv = document.createElement("div");
+      const innerDiv = document.createElement("div");
       const h2 = document.createElement("h2");
       const thumbnail = document.createElement("img");
+      const p = document.createElement("p");
       const link = document.createElement("a");
       h2.innerText = post.data.title;
       thumbnail.src = post.data.thumbnail;
       link.href = post.data.url;
       link.innerText = "link to post";
-      console.log(post.data.title);
+      p.innerText = post.data.selftext;
+      //   console.log(post.data.selftext);
       postDiv.appendChild(h2);
       postDiv.appendChild(thumbnail);
+      postDiv.append(p);
+      postDiv.appendChild(innerDiv);
       postDiv.appendChild(link);
       document.body.appendChild(postDiv);
     });
