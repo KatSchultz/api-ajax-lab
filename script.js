2a4863bc4e290f92f01311ee09532e4b8eaf10f5
KatSchultz <ks.katieschultz@gmail.com>
2022-06-07 20:27:37 -0400

    displays new subreddit posts, more styling

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
-fetch("https://www.reddit.com/r/aww/.json")
+const submitBtn = document.getElementById("submit");
+const userInput = document.getElementById("input");
+
+const getData = fetch("https://www.reddit.com/r/aww/.json")
   .then((res) => res.json())
   .then((jsonData) => {
-    let posts = jsonData.data.children;
-    let first10 = posts.slice(0, 10);
-    console.log(first10);
-    first10.forEach((post) => {
-      //   console.log(post.data);
-      const postDiv = document.createElement("div");
-      const innerDiv = document.createElement("div");
-      const h2 = document.createElement("h2");
-      const thumbnail = document.createElement("img");
-      const p = document.createElement("p");
-      const link = document.createElement("a");
-      h2.innerText = post.data.title;
-      thumbnail.src = post.data.thumbnail;
-      link.href = post.data.url;
-      link.innerText = "link to post";
-      p.innerText = post.data.selftext;
-      //   console.log(post.data.selftext);
-      postDiv.appendChild(h2);
-      postDiv.appendChild(thumbnail);
-      postDiv.append(p);
-      postDiv.appendChild(innerDiv);
-      postDiv.appendChild(link);
-      document.body.appendChild(postDiv);
-    });
+    return jsonData;
   });
+
+//FIRST DISPLAY OF AWW
+const displayPosts = getData.then((jsonData) => {
+  let posts = jsonData.data.children;
+  let first10 = posts.slice(0, 10);
+  first10.forEach((post) => {
+    const postDiv = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const thumbnail = document.createElement("img");
+    const link = document.createElement("a");
+    h2.innerText = post.data.title;
+    thumbnail.src = post.data.thumbnail;
+    link.href = post.data.url;
+    link.innerText = "link to post";
+    postDiv.appendChild(h2);
+    postDiv.appendChild(thumbnail);
+    postDiv.appendChild(link);
+    document.body.appendChild(postDiv);
+  });
+  return jsonData;
+});
+
+//CLICK HANDLER
+submitBtn.addEventListener("click", () => {
+  let newSub = input.value;
+  const getNewData = fetch(`https://www.reddit.com/r/${newSub}/.json`)
+    .then((res) => res.json())
+    .then((jsonData) => {
+      let posts = jsonData.data.children;
+      let first10 = posts.slice(0, 10);
+      for (let i = 0; i < first10.length; i++) {
+        document.body.lastChild.remove();
+      }
+      first10.forEach((post) => {
+        const postDiv = document.createElement("div");
+        const h2 = document.createElement("h2");
+        const thumbnail = document.createElement("img");
+        const link = document.createElement("a");
+        h2.innerText = post.data.title;
+        thumbnail.src = post.data.thumbnail;
+        link.href = post.data.url;
+        link.innerText = "link to post";
+        postDiv.appendChild(h2);
+        postDiv.appendChild(thumbnail);
+        postDiv.appendChild(link);
+        document.body.appendChild(postDiv);
+      });
+      return jsonData;
+    });
+});
